d5c4fd13c59cbd688b8dced84558cf1dfa7df695
Karabo Masalesa <kaaribu@protonmail.com>
2023-02-16 12:40:05 +0200

    Updated Apiservice

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  postTask(data: any){
+    return this.http.post<any>("http://localhost:3000/taskList", data);
+  }
+  getTask(){
+    return this.http.get<any>("http://localhost:3000/taskList");
+  }
 }
