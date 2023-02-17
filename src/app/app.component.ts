21a8baef967314dcc38d306e079e5b5fa246bd4e
Karabo Masalesa <kaaribu@protonmail.com>
2023-02-17 19:34:20 +0200

    Added app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,82 +1,12 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {DialogComponent} from "./dialog/dialog.component";
-import {ApiService} from "./services/api.service";
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'angular-todo';
-
-  displayedColumns: string[] = ['taskName', 'category', 'date', 'priority', 'description', 'action'];
-  dataSource!: MatTableDataSource<any>;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
-  constructor(private dialog: MatDialog, private api: ApiService) {
-  }
-
-  ngOnInit(): void {
-    this.getAllTasks();
-  }
-
-  OpenDialog() {
-    this.dialog.open(DialogComponent, {
-      width: '30%',
-    }).afterClosed().subscribe(val => {
-      if (val === 'save') {
-        this.getAllTasks();
-      }
-    });
-  }
-
-  getAllTasks() {
-    this.api.getTask().subscribe({next:(res) => {
-      this.dataSource = new MatTableDataSource(res);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    },
-    error: (err) => {
-      alert("Error while fetching tasks!");
-    }
-    })
-  }
-
-  editTask(row: any) {
-    this.dialog.open(DialogComponent, {
-      width: '30%',
-      data: row
-    }).afterClosed().subscribe(val => {
-      if (val === 'update') {
-        this.getAllTasks();
-      }
-    });
-  }
-
-  deleteTask(id: number) {
-    this.api.deleteTask(id).subscribe({next:(res) => {
-      alert("Task deleted successfully!");
-      this.getAllTasks();
-    },
-    error: () => {
-      alert("Error while deleting task!");
-    }
-    })
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
 }
+
+
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatDialog} from "@angular/material/dialog";
+import {ApiService} from "../services/api.service";
+import {DialogComponent} from "../dialog/dialog.component";
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
 
+  displayedColumns: string[] = ['taskName', 'category', 'date', 'priority', 'description', 'action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dialog: MatDialog, private api: ApiService) {
+  }
+
+  ngOnInit(): void {
+    this.getAllTasks();
+  }
+
+  OpenDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+    }).afterClosed().subscribe(val => {
+      if (val === 'save') {
+        this.getAllTasks();
+      }
+    });
+  }
+
+  getAllTasks() {
+    this.api.getTask().subscribe({next:(res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        alert("Error while fetching tasks!");
+      }
+    })
+  }
+
+  editTask(row: any) {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+      data: row
+    }).afterClosed().subscribe(val => {
+      if (val === 'update') {
+        this.getAllTasks();
+      }
+    });
+  }
+
+  deleteTask(id: number) {
+    this.api.deleteTask(id).subscribe({next:(res) => {
+        alert("Task deleted successfully!");
+        this.getAllTasks();
+      },
+      error: () => {
+        alert("Error while deleting task!");
+      }
+    })
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
